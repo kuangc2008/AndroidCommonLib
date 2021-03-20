abec6317ed734a1f2b79240a6f2d972336e10068
kuangcheng <hikuangc@gmail.com>
2021-03-20 12:15:24 +0800

    ToastUtil

--- app/src/main/java/com/kc/androidlib/MainActivity.kt
+++ app/src/main/java/com/kc/androidlib/MainActivity.kt
@@ -2,10 +2,16 @@ package com.kc.androidlib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.kc.androidlib.toast.Toasty
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.toast_utils).setOnClickListener {
+            Toasty.info(MainActivity@this, "1111").show()
+        }
     }
 }
